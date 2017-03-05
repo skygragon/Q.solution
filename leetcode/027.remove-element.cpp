23d77d5ff2351842bf243145056277eb64993740
Eric Wang <skygragon@gmail.com>
2017-03-05 11:42:45 +0800

    leetcode 027
    Signed-off-by: Eric Wang <skygragon@gmail.com>

---
+++ leetcode/027.remove-element.cpp
@@ -0,0 +1,44 @@
+/*
+ * [27] Remove Element
+ *
+ * https://leetcode.com/problems/remove-element
+ *
+ * Easy (37.52%)
+ * Total Accepted:    177537
+ * Total Submissions: 473168
+ * Testcase Example:  '[3,2,2,3]\n3'
+ *
+ * Given an array and a value, remove all instances of that value in place and
+ * return the new length.
+ * 
+ * 
+ * Do not allocate extra space for another array, you must do this in place
+ * with constant memory.
+ * 
+ * The order of elements can be changed. It doesn't matter what you leave
+ * beyond the new length.
+ * 
+ * 
+ * Example:
+ * Given input array nums = [3,2,2,3], val = 3
+ * 
+ * 
+ * Your function should return length = 2, with the first two elements of nums
+ * being 2.
+ * 
+ * 
+ * Try two pointers.
+ * Did you use the property of "the order of elements can be changed"?
+ * What happens when the elements to remove are rare?
+ * 
+ */
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != val) nums[k++] = nums[i];
+        }
+        return k;
+    }
+};
