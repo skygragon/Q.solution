13ab3d5cf6ff992e54623c7922093b811d9b873e
Eric Wang <skygragon@gmail.com>
2017-03-05 12:29:10 +0800

    leetcode 100
    Signed-off-by: Eric Wang <skygragon@gmail.com>

---
+++ leetcode/100.same-tree.cpp
@@ -0,0 +1,38 @@
+/*
+ * [100] Same Tree
+ *
+ * https://leetcode.com/problems/same-tree
+ *
+ * Easy (45.56%)
+ * Total Accepted:    185510
+ * Total Submissions: 407133
+ * Testcase Example:  '[]\n[]'
+ *
+ * 
+ * Given two binary trees, write a function to check if they are equal or
+ * not.
+ * 
+ * 
+ * Two binary trees are considered equal if they are structurally identical and
+ * the nodes have the same value.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        if (!p || !q) return false;
+        return (p->val == q->val) &&
+               isSameTree(p->left, q->left) &&
+               isSameTree(p->right, q->right);
+    }
+};
