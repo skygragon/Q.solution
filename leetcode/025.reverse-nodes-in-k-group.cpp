ba40eacf8288a8e78324f3226f1ce798ada5249c
Eric Wang <skygragon@gmail.com>
2017-03-07 20:15:47 +0800

    leetcode 025
    Signed-off-by: Eric Wang <skygragon@gmail.com>

---
+++ leetcode/025.reverse-nodes-in-k-group.cpp
@@ -0,0 +1,76 @@
+/*
+ * [25] Reverse Nodes in k-Group
+ *
+ * https://leetcode.com/problems/reverse-nodes-in-k-group
+ *
+ * Hard (30.02%)
+ * Total Accepted:    84984
+ * Total Submissions: 283060
+ * Testcase Example:  '[]\n1'
+ *
+ * 
+ * Given a linked list, reverse the nodes of a linked list k at a time and
+ * return its modified list.
+ * 
+ * 
+ * 
+ * k is a positive integer and is less than or equal to the length of the
+ * linked list. If the number of nodes is not a multiple of k then left-out
+ * nodes in the end should remain as it is.
+ * 
+ * You may not alter the values in the nodes, only nodes itself may be
+ * changed.
+ * 
+ * Only constant memory is allowed.
+ * 
+ * 
+ * For example,
+ * Given this linked list: 1->2->3->4->5
+ * 
+ * 
+ * 
+ * For k = 2, you should return: 2->1->4->3->5
+ * 
+ * 
+ * 
+ * For k = 3, you should return: 3->2->1->4->5
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (k == 1) return head;
+
+        int n = 0;
+        ListNode* cur = head;
+        while (cur) { ++n; cur = cur->next; }
+        if (n < k) return head;
+
+        ListNode node(0);
+        ListNode* curHead = &node;
+
+        cur = head;
+        while (n >= k) {
+            ListNode* curEnd = cur;
+            for (int i = 0; i < k; ++i) {
+                ListNode* next = cur->next;
+                cur->next = curHead->next;
+                curHead->next = cur;
+                cur = next;
+            }
+            curHead = curEnd;
+            n -= k;
+        }
+        curHead->next = cur;
+
+        return node.next;
+    }
+};
