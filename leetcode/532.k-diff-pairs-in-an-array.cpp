ff096f59cfad05b034cc9de934c594321588f66e
Eric Wang <skygragon@gmail.com>
2017-03-11 22:46:03 +0800

    leetcode 532
    Signed-off-by: Eric Wang <skygragon@gmail.com>

---
+++ leetcode/532.k-diff-pairs-in-an-array.cpp
@@ -0,0 +1,73 @@
+/*
+ * [532] K-diff Pairs in an Array
+ *
+ * https://leetcode.com/problems/k-diff-pairs-in-an-array
+ *
+ * Easy (25.20%)
+ * Total Accepted:    4168
+ * Total Submissions: 15974
+ * Testcase Example:  '[3,1,4,1,5]\n2'
+ *
+ * 
+ * Given an array of integers and an integer k, you need to find the number of
+ * unique k-diff pairs in the array. Here a k-diff pair is defined as an
+ * integer pair (i, j), where i and j are both numbers in the array and their
+ * absolute difference is k.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: [3, 1, 4, 1, 5], k = 2
+ * Output: 2
+ * Explanation: There are two 2-diff pairs in the array, (1, 3) and (3,
+ * 5).Although we have two 1s in the input, we should only return the number of
+ * unique pairs.
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input:[1, 2, 3, 4, 5], k = 1
+ * Output: 4
+ * Explanation: There are four 1-diff pairs in the array, (1, 2), (2, 3), (3,
+ * 4) and (4, 5).
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * Input: [1, 3, 1, 5, 4], k = 0
+ * Output: 1
+ * Explanation: There is one 0-diff pair in the array, (1, 1).
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * The pairs (i, j) and (j, i) count as the same pair.
+ * The length of the array won't exceed 10,000.
+ * All the integers in the given input belong to the range: [-1e7, 1e7].
+ * 
+ * 
+ */
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size(), x = 0;
+        for (int i = 0; i < n-1; ++i) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+
+            int j = i+1;
+            while (j < n) {
+                if (nums[j] - nums[i] == k) { ++x; break; }
+                if (nums[j] - nums[i] > k) break;
+                ++j;
+            }
+        }
+
+        return x;
+    }
+};
