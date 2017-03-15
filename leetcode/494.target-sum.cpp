bcb66fd181ee35a1eb61cb6259b2b9930842dac0
Eric Wang <skygragon@gmail.com>
2017-03-15 12:15:28 +0800

    leetcode 494
    Signed-off-by: Eric Wang <skygragon@gmail.com>

---
+++ leetcode/494.target-sum.cpp
@@ -0,0 +1,61 @@
+/*
+ * [494] Target Sum
+ *
+ * https://leetcode.com/problems/target-sum
+ *
+ * Medium (44.15%)
+ * Total Accepted:    10201
+ * Total Submissions: 23110
+ * Testcase Example:  '[1,1,1,1,1]\n3'
+ *
+ * 
+ * You are given a list of non-negative integers, a1, a2, ..., an, and a
+ * target, S. Now you have 2 symbols + and -. For each integer, you should
+ * choose one from + and - as its new symbol.
+ * ⁠
+ * 
+ * Find out how many ways to assign symbols to make sum of integers equal to
+ * target S.  
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: nums is [1, 1, 1, 1, 1], S is 3. 
+ * Output: 5
+ * Explanation: 
+ * 
+ * -1+1+1+1+1 = 3
+ * +1-1+1+1+1 = 3
+ * +1+1-1+1+1 = 3
+ * +1+1+1-1+1 = 3
+ * +1+1+1+1-1 = 3
+ * 
+ * There are 5 ways to assign symbols to make the sum of nums be target 3.
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * The length of the given array is positive and will not exceed 20. 
+ * The sum of elements in the given array will not exceed 1000.
+ * Your output answer is guaranteed to be fitted in a 32-bit integer.
+ * 
+ * 
+ */
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int S) {
+        int n = nums.size();
+        vector<unordered_map<int, int>> v(n+1);
+        v[0].insert({0, 1});
+
+        for (int i=0; i < n; ++i) {
+            for (auto p : v[i]) {
+                v[i+1][p.first+nums[i]] += p.second;
+                v[i+1][p.first-nums[i]] += p.second;
+            }
+        }
+
+        return v[n][S];
+    }
+};
