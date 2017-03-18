d46e52a54c0661884ad761f2f7aaa1c0efc05b6f
Eric Wang <skygragon@gmail.com>
2017-03-18 15:53:35 +0800

    leetcode 078
    Signed-off-by: Eric Wang <skygragon@gmail.com>

---
+++ leetcode/078.subsets.cpp
@@ -0,0 +1,60 @@
+/*
+ * [78] Subsets
+ *
+ * https://leetcode.com/problems/subsets
+ *
+ * Medium (37.73%)
+ * Total Accepted:    145292
+ * Total Submissions: 385000
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 
+ * Given a set of distinct integers, nums, return all possible subsets.
+ * 
+ * Note: The solution set must not contain duplicate subsets.
+ * 
+ * 
+ * For example,
+ * If nums = [1,2,3], a solution is:
+ * 
+ * 
+ * 
+ * [
+ * ⁠ [3],
+ * ⁠ [1],
+ * ⁠ [2],
+ * ⁠ [1,2,3],
+ * ⁠ [1,3],
+ * ⁠ [2,3],
+ * ⁠ [1,2],
+ * ⁠ []
+ * ]
+ * 
+ */
+class Solution {
+public:
+    vector<vector<int>> vv;
+    vector<int> v, cur;
+    int n;
+
+    void next(int i) {
+        if (i == n) {
+            vv.push_back(cur);
+            return;
+        }
+
+        cur.push_back(v[i]);
+        next(i+1);
+        cur.pop_back();
+        next(i+1);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        v = nums;
+        n = v.size();
+
+        next(0);
+
+        return vv;
+    }
+};
