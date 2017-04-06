685e766ae5b08bfebd54ccb3362211157830af76
Eric Wang <skygragon@gmail.com>
2017-04-06 16:06:58 +0800

    leetcode 018
    Signed-off-by: Eric Wang <skygragon@gmail.com>

---
+++ leetcode/018.4sum.cpp
@@ -0,0 +1,58 @@
+/*
+ * [18] 4Sum
+ *
+ * https://leetcode.com/problems/4sum
+ *
+ * Medium (25.95%)
+ * Total Accepted:    106405
+ * Total Submissions: 409946
+ * Testcase Example:  '[1,0,-1,0,-2,2]\n0'
+ *
+ * Given an array S of n integers, are there elements a, b, c, and d in S such
+ * that a + b + c + d = target? Find all unique quadruplets in the array which
+ * gives the sum of target.
+ * 
+ * Note: The solution set must not contain duplicate quadruplets.
+ * 
+ * 
+ * 
+ * For example, given array S = [1, 0, -1, 0, -2, 2], and target = 0.
+ * 
+ * A solution set is:
+ * [
+ * ⁠ [-1,  0, 0, 1],
+ * ⁠ [-2, -1, 1, 2],
+ * ⁠ [-2,  0, 0, 2]
+ * ]
+ * 
+ */
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        vector<vector<int>> vv;
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n-3; ++i) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            for (int j = i+1; j < n-2; ++j) {
+                int s = nums[i]+nums[j];
+                if (j > i+1 && nums[j] == nums[j-1]) continue;
+                if (s+nums[j+1]+nums[j+2] > target) break;
+                if (s+nums[n-2]+nums[n-1] < target) continue;
+
+                int l = j+1, r = n-1;
+                while (l < r) {
+                    if (nums[l]+nums[r] > target-s) --r;
+                    else if (nums[l]+nums[r] < target-s) ++l;
+                    else { vv.push_back({nums[i], nums[j], nums[l], nums[r]}); ++l; }
+
+                    while (l > j+1 && l < r && nums[l] == nums[l-1]) ++l;
+                    while (r < n-1 && l < r && nums[r] == nums[r+1]) --r;
+                }
+            }
+        }
+        return vv;
+    }
+};
