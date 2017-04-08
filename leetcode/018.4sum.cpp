aed7e79a638a0250bf9323bb69241475314e763b
Eric Wang <skygragon@gmail.com>
2017-04-08 10:48:27 +0800

    refactor leetcode 018
    Signed-off-by: Eric Wang <skygragon@gmail.com>

--- leetcode/018.4sum.cpp
+++ leetcode/018.4sum.cpp
@@ -29,27 +29,27 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n = nums.size();
-
         vector<vector<int>> vv;
         sort(nums.begin(), nums.end());
 
-        for (int i = 0; i < n-3; ++i) {
+        for (int i = 0, n = nums.size(); i < n-3; ++i) {
             if (i > 0 && nums[i] == nums[i-1]) continue;
+
             for (int j = i+1; j < n-2; ++j) {
-                int s = nums[i]+nums[j];
+                int sum = nums[i]+nums[j];
                 if (j > i+1 && nums[j] == nums[j-1]) continue;
-                if (s+nums[j+1]+nums[j+2] > target) break;
-                if (s+nums[n-2]+nums[n-1] < target) continue;
+
+                if (sum+nums[j+1]+nums[j+2] > target) break;
+                if (sum+nums[n-2]+nums[n-1] < target) continue;
 
                 int l = j+1, r = n-1;
                 while (l < r) {
-                    if (nums[l]+nums[r] > target-s) --r;
-                    else if (nums[l]+nums[r] < target-s) ++l;
+                    if (sum+nums[l]+nums[r] > target) --r;
+                    else if (sum+nums[l]+nums[r] < target) ++l;
                     else { vv.push_back({nums[i], nums[j], nums[l], nums[r]}); ++l; }
 
-                    while (l > j+1 && l < r && nums[l] == nums[l-1]) ++l;
-                    while (r < n-1 && l < r && nums[r] == nums[r+1]) --r;
+                    while (l < r && l > j+1 && nums[l] == nums[l-1]) ++l;
+                    while (l < r && r < n-1 && nums[r] == nums[r+1]) --r;
                 }
             }
         }
