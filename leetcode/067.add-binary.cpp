9d42f8512cf39000c9308ceabdd97e74e4c5fd00
Eric Wang <skygragon@gmail.com>
2017-04-09 10:15:02 +0800

    refactor leetcode 067
    Signed-off-by: Eric Wang <skygragon@gmail.com>

--- leetcode/067.add-binary.cpp
+++ leetcode/067.add-binary.cpp
@@ -22,19 +22,16 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int i = a.size()-1;
-        int j = b.size()-1;
-        int x = 0;
+        int i = a.size()-1, j = b.size()-1, sum = 0;
 
         string s;
-        while (i >= 0 || j >= 0 || x > 0) {
-            x += (i >= 0) ? a[i] - '0' : 0;
-            x += (j >= 0) ? b[j] - '0' : 0;
+        while (i >= 0 || j >= 0 || sum > 0) {
+            if (i >= 0) sum += a[i--]-'0';
+            if (j >= 0) sum += b[j--]-'0';
 
-            s = to_string(x % 2) + s;
-            x /= 2; --i; --j;
+            s = to_string(sum%2) + s;
+            sum /= 2;
         }
-
         return s;
     }
 };
