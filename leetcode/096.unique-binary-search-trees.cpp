4ef30ef69a00364c6807bc30c02cc321c6843ce5
Eric Wang <skygragon@gmail.com>
2017-04-11 12:42:58 +0800

    refactor leetcode 096
    Signed-off-by: Eric Wang <skygragon@gmail.com>

--- leetcode/096.unique-binary-search-trees.cpp
+++ leetcode/096.unique-binary-search-trees.cpp
@@ -27,13 +27,10 @@
 class Solution {
 public:
     int numTrees(int n) {
-        vector<int> v(n+1, 0);
-        v[1] = 1;
-        for (int i = 2; i <= n; ++i) {
-            v[i] = 2*v[i-1];
-            for (int j = 0; j < i-1; ++j)
-                v[i] += v[j]*v[i-1-j];
-        }
-        return v[n];
+        vector<int> count(n+1, 0);
+        count[0] = 1;
+        for (int i = 1; i <= n; ++i)
+            for (int j = 0; j < i; ++j) count[i] += count[j]*count[i-1-j];
+        return count[n];
     }
 };
