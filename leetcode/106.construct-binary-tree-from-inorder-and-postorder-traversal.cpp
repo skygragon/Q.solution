cc927813dcaa5234fa63e2a8ace52bdbf61a583f
Eric Wang <skygragon@gmail.com>
2017-04-11 12:32:35 +0800

    refactor leetcode 106
    Signed-off-by: Eric Wang <skygragon@gmail.com>

--- leetcode/106.construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ leetcode/106.construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -30,22 +30,19 @@ public:
     TreeNode* next(int i1, int j1, int i2, int j2) {
         if (i1 > j1 || i2 > j2) return NULL;
 
-        int x = post[j2];
-        int k;
+        int x = post[j2], k;
         for (k = i1; k <= j1 && in[k] != x; ++k);
 
         TreeNode* p = new TreeNode(x);
         p->left = next(i1, k-1, i2, i2+k-1-i1);
         p->right = next(k+1, j1, j2+k-j1, j2-1);
-
         return p;
     }
 
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         if (inorder.empty() || postorder.empty()) return NULL;
 
-        in = inorder;
-        post = postorder;
+        in = inorder; post = postorder;
         return next(0, in.size()-1, 0, post.size()-1);
     }
 };
