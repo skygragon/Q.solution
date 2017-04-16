6bf4613311afc6ba440c91a9410cc4c0212d05ce
Eric Wang <skygragon@gmail.com>
2017-04-16 11:44:37 +0800

    refactor leetcode 021
    Signed-off-by: Eric Wang <skygragon@gmail.com>

--- leetcode/021.merge-two-sorted-lists.cpp
+++ leetcode/021.merge-two-sorted-lists.cpp
@@ -22,19 +22,12 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode node(0);
-        ListNode* p = &node;
-
-        while (l1 && l2) {
-            if (l1->val < l2->val) {
-                p->next = l1; l1 = l1->next; p = p->next;
-            } else {
-                p->next = l2; l2 = l2->next; p = p->next;
-            }
+        ListNode node(0), *p = &node;
+        for (; l1 && l2; p = p->next) {
+            if (l1->val < l2->val) { p->next = l1; l1 = l1->next; }
+            else { p->next = l2; l2 = l2->next; }
         }
-        while (l1) { p->next = l1; l1 = l1->next; p = p->next; }
-        while (l2) { p->next = l2; l2 = l2->next; p = p->next; }
-
+        p->next = l1 ? l1 : l2;
         return node.next;
     }
 };
