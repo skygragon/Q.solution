a44c750785ade1e070aa84d27777aa97569c0dd1
Eric Wang <skygragon@gmail.com>
2017-04-16 12:22:31 +0800

    refactor leetcode 041
    Signed-off-by: Eric Wang <skygragon@gmail.com>

--- leetcode/041.first-missing-positive.cpp
+++ leetcode/041.first-missing-positive.cpp
@@ -26,19 +26,12 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] == i+1) continue;
-            if (nums[i] <= 0) continue;
-            while (nums[i] > 0 && nums[i] <= n) {
-                int j = nums[i]-1;
-                if (nums[j] == j+1) break;
-                swap(nums[i], nums[j]);
+        for (int i = 0; i < n; ++i)
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i]-1] != nums[i]) {
+                swap(nums[i], nums[nums[i]-1]);
             }
-        }
 
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] != i+1) return i+1;
-        }
+        for (int i = 0; i < n; ++i) if (nums[i] != i+1) return i+1;
         return n+1;
     }
 };
