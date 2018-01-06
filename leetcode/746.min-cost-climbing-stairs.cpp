841754a0dc460253ee3ef903a9344bb86a25be11
skygragon <skygragon@gmail.com>
2018-01-06 15:09:39 +0800

    update 746.min-cost-climbing-stairs.cpp

--- leetcode/746.min-cost-climbing-stairs.cpp
+++ leetcode/746.min-cost-climbing-stairs.cpp
@@ -47,9 +47,7 @@ class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         size_t n = cost.size();
-        for (int i = 2; i < n; ++i) {
-            cost[i] += min(cost[i-1], cost[i-2]);
-        }
+        for (int i = 2; i < n; ++i) cost[i] += min(cost[i-1], cost[i-2]);
         return min(cost[n-1], cost[n-2]);
     }
 };
