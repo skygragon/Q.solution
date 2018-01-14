b666e47facb3c42299ae5bf5705d753ca7eae3d1
skygragon <skygragon@gmail.com>
2018-01-14 12:48:26 +0800

    update 728.self-dividing-numbers.cpp

---
+++ leetcode/728.self-dividing-numbers.cpp
@@ -0,0 +1,55 @@
+/*
+ * [728] Self Dividing Numbers
+ *
+ * https://leetcode.com/problems/self-dividing-numbers/description/
+ *
+ * algorithms
+ * Easy (67.53%)
+ * Total Accepted:    16.4K
+ * Total Submissions: 24.2K
+ * Testcase Example:  '1\n22'
+ *
+ * 
+ * A self-dividing number is a number that is divisible by every digit it
+ * contains.
+ * 
+ * For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 ==
+ * 0, and 128 % 8 == 0.
+ * 
+ * Also, a self-dividing number is not allowed to contain the digit zero.
+ * 
+ * Given a lower and upper number bound, output a list of every possible self
+ * dividing number, including the bounds if possible.
+ * 
+ * Example 1:
+ * 
+ * Input: 
+ * left = 1, right = 22
+ * Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+ * 
+ * 
+ * 
+ * Note:
+ * The boundaries of each input argument are 1 .
+ * 
+ */
+class Solution {
+public:
+    bool isSelfDividing(int x) {
+        int y = x;
+        while (y) {
+            int m = y % 10;
+            if (m == 0) return false;
+            if (x % m != 0) return false;
+            y = y / 10;
+        }
+        return true;
+    }
+
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> v;
+        for (int i = left; i <= right; ++i)
+            if (isSelfDividing(i)) v.push_back(i);
+        return v;
+    }
+};
