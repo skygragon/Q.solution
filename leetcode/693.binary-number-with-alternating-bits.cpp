920df1bedea337a346fd65922cb2282cfda1fca2
skygragon <skygragon@gmail.com>
2018-01-14 13:53:47 +0800

    update 693.binary-number-with-alternating-bits.cpp

---
+++ leetcode/693.binary-number-with-alternating-bits.cpp
@@ -0,0 +1,65 @@
+/*
+ * [693] Binary Number with Alternating Bits
+ *
+ * https://leetcode.com/problems/binary-number-with-alternating-bits/description/
+ *
+ * algorithms
+ * Easy (55.47%)
+ * Total Accepted:    13.7K
+ * Total Submissions: 24.7K
+ * Testcase Example:  '5'
+ *
+ * Given a positive integer, check whether it has alternating bits: namely, if
+ * two adjacent bits will always have different values.
+ * 
+ * Example 1:
+ * 
+ * Input: 5
+ * Output: True
+ * Explanation:
+ * The binary representation of 5 is: 101
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: 7
+ * Output: False
+ * Explanation:
+ * The binary representation of 7 is: 111.
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * Input: 11
+ * Output: False
+ * Explanation:
+ * The binary representation of 11 is: 1011.
+ * 
+ * 
+ * 
+ * Example 4:
+ * 
+ * Input: 10
+ * Output: True
+ * Explanation:
+ * The binary representation of 10 is: 1010.
+ * 
+ * 
+ */
+class Solution {
+public:
+    bool isAlternative(long long x) {
+        x = x*3+1;
+        while (x > 1) {
+            if (x % 4) return false;
+            x /= 4;
+        }
+        return true;
+    }
+
+    bool hasAlternatingBits(int n) {
+        return n % 2 ? isAlternative(n) : isAlternative(n/2);    
+    }
+};
