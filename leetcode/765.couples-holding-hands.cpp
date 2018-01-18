8a08cc5a8024ba7b02ce4a9eef2203b16d39bde5
skygragon <skygragon@gmail.com>
2018-01-18 21:23:10 +0800

    update 765.couples-holding-hands.cpp

---
+++ leetcode/765.couples-holding-hands.cpp
@@ -0,0 +1,62 @@
+/*
+ * [770] Couples Holding Hands
+ *
+ * https://leetcode.com/problems/couples-holding-hands/description/
+ *
+ * algorithms
+ * Hard (46.50%)
+ * Total Accepted:    1K
+ * Total Submissions: 2.2K
+ * Testcase Example:  '[0,2,1,3]'
+ *
+ * 
+ * N couples sit in 2N seats arranged in a row and want to hold hands.  We want
+ * to know the minimum number of swaps so that every couple is sitting side by
+ * side.  A swap consists of choosing any two people, then they stand up and
+ * switch seats. 
+ * 
+ * The people and seats are represented by an integer from 0 to 2N-1, the
+ * couples are numbered in order, the first couple being (0, 1), the second
+ * couple being (2, 3), and so on with the last couple being (2N-2, 2N-1).
+ * 
+ * The couples' initial seating is given by row[i] being the value of the
+ * person who is initially sitting in the i-th seat.
+ * 
+ * Example 1:
+ * Input: row = [0, 2, 1, 3]
+ * Output: 1
+ * Explanation: We only need to swap the second (row[1]) and third (row[2])
+ * person.
+ * 
+ * 
+ * Example 2:
+ * Input: row = [3, 2, 0, 1]
+ * Output: 0
+ * Explanation: All couples are already seated side by side.
+ * 
+ * 
+ * 
+ * Note:
+ * ⁠
+ * ⁠len(row) is even and in the range of [4, 60].
+ * ⁠row is guaranteed to be a permutation of 0...len(row)-1.
+ * 
+ */
+class Solution {
+public:
+    int minSwapsCouples(vector<int>& row) {
+        int x = 0;
+        for (int i = 0, n = row.size(); i < n; i += 2) {
+            int y = row[i] % 2 ? row[i]-1 : row[i]+1;
+            for (int j = i+1; j < n; ++j) {
+                if (row[j] == y) {
+                    swap(row[j], row[i+1]);
+                    if (j != i+1) ++x;
+                    break;
+                }
+            }
+        }
+        return x;
+    }
+};
+
