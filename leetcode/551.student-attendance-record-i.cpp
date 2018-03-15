76d63c38ec2687de9dd1658474fd640269ccbb69
skygragon <skygragon@gmail.com>
2018-03-15 13:54:48 +0800

    update 551.student-attendance-record-i.cpp

---
+++ leetcode/551.student-attendance-record-i.cpp
@@ -0,0 +1,58 @@
+/*
+ * [551] Student Attendance Record I
+ *
+ * https://leetcode.com/problems/student-attendance-record-i/description/
+ *
+ * algorithms
+ * Easy (44.49%)
+ * Total Accepted:    29.5K
+ * Total Submissions: 66.3K
+ * Testcase Example:  '"PPALLP"'
+ *
+ * You are given a string representing an attendance record for a student. The
+ * record only contains the following three characters:
+ * 
+ * 
+ * 
+ * 'A' : Absent. 
+ * 'L' : Late.
+ * ⁠'P' : Present. 
+ * 
+ * 
+ * 
+ * 
+ * A student could be rewarded if his attendance record doesn't contain more
+ * than one 'A' (absent) or more than two continuous 'L' (late).    
+ * 
+ * You need to return whether the student could be rewarded according to his
+ * attendance record.
+ * 
+ * Example 1:
+ * 
+ * Input: "PPALLP"
+ * Output: True
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: "PPALLL"
+ * Output: False
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int a = 0, l = 0;
+        for (auto c: s) {
+            if (c == 'A') ++a;
+            if (c == 'L') ++l; else l = 0;
+            if (a == 2 || l == 3) return false;
+        }
+        return true;
+    }
+};
