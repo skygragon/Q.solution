7cdc923cc70ae2d5074ba04b30d572179e3e8573
skygragon <skygragon@gmail.com>
2018-03-15 15:58:14 +0800

    update 687.longest-univalue-path.cpp

---
+++ leetcode/687.longest-univalue-path.cpp
@@ -0,0 +1,99 @@
+/*
+ * [687] Longest Univalue Path
+ *
+ * https://leetcode.com/problems/longest-univalue-path/description/
+ *
+ * algorithms
+ * Easy (33.08%)
+ * Total Accepted:    20.6K
+ * Total Submissions: 62.5K
+ * Testcase Example:  '[5,4,5,1,1,5]'
+ *
+ * Given a binary tree, find the length of the longest path where each node in
+ * the path has the same value. This path may or may not pass through the
+ * root.
+ * 
+ * Note: The length of path between two nodes is represented by the number of
+ * edges between them.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * 
+ * 
+ * Input:
+ * 
+ * ⁠             5
+ * ⁠            / \
+ * ⁠           4   5
+ * ⁠          / \   \
+ * ⁠         1   1   5
+ * 
+ * 
+ * 
+ * 
+ * Output:
+ * 
+ * 2
+ * 
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * 
+ * 
+ * Input:
+ * 
+ * ⁠             1
+ * ⁠            / \
+ * ⁠           4   5
+ * ⁠          / \   \
+ * ⁠         4   4   5
+ * 
+ * 
+ * 
+ * 
+ * Output:
+ * 
+ * 2
+ * 
+ * 
+ * 
+ * Note:
+ * The given binary tree has not more than 10000 nodes.  The height of the tree
+ * is not more than 1000.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int x = 0;
+
+    int next(TreeNode *p) {
+        if (!p) return 0;
+
+        int l = next(p->left);
+        l = (p->left && p->val == p->left->val) ? l+1 : 0;
+        int r = next(p->right);
+        r = (p->right && p->val == p->right->val) ? r+1 : 0;
+
+        if (l+r > x) x = l+r;
+        return max(l, r);
+    }
+
+    int longestUnivaluePath(TreeNode* root) {
+        next(root);
+        return x;
+    }
+};
