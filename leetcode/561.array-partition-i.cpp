ecb9e7f0c5faec4aff9cdbfb4aa7ba6abf618fde
skygragon <skygragon@gmail.com>
2018-07-09 14:13:24 +0800

    update 561.array-partition-i.cpp

---
+++ leetcode/561.array-partition-i.cpp
@@ -0,0 +1,43 @@
+/*
+ * [561] Array Partition I
+ *
+ * https://leetcode.com/problems/array-partition-i
+ *
+ * algorithms
+ * Easy (67.08%)
+ * Total Accepted:    36.2K
+ * Total Submissions: 54K
+ * Testcase Example:  '[1,4,3,2]'
+ *
+ * 
+ * Given an array of 2n integers, your task is to group these integers into n
+ * pairs of integer, say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of
+ * min(ai, bi) for all i from 1 to n as large as possible.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: [1,4,3,2]
+ * 
+ * Output: 4
+ * Explanation: n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3,
+ * 4).
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * n is a positive integer, which is in the range of [1, 10000].
+ * All the integers in the array will be in the range of [-10000, 10000].
+ * 
+ * 
+ */
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int x = 0, n = nums.size();
+        for (int i = 0; i < n; i += 2) x += nums[i];
+        return x;
+    }
+};
